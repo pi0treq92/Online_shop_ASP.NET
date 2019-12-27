ce90ab1c4630c2b0b25b83eec7a14a6e5b4574f4
pi0treq92 <32812870+pi0treq92@users.noreply.github.com>
2019-12-27 08:35:35 +0100

    sitemap

--- twoja-manufaktura/twoja-manufaktura/Controllers/StoreController.cs
+++ twoja-manufaktura/twoja-manufaktura/Controllers/StoreController.cs
@@ -3,23 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using twoja_manufaktura.DAL;
 
 namespace twoja_manufaktura.Controllers
 {
     public class StoreController : Controller
     {
         // GET: Store
+        StoreContext db = new StoreContext();
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult Details(int id)
         {
-            return View();
+            var product = db.Products.Find(id);
+            return View(product);
         }
         public ActionResult List(string genrename)
         {
-            return View();
+            var genre = db.Genres.Include("Products").Where(g => g.Name.ToUpper() == genrename.ToUpper()).Single();
+            var products = genre.Products.ToList();
+            return View(products);
+        }
+        [ChildActionOnly]
+        public ActionResult GenresMenu()
+        {
+            var genres = db.Genres.ToList();
+            return PartialView("_GenresMenu", genres);
         }
 
     }
---
+++ twoja-manufaktura/twoja-manufaktura/Infrastructure/ProductDetailsDynamicNodeProvider.cs
@@ -0,0 +1,31 @@
+﻿using MvcSiteMapProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using twoja_manufaktura.DAL;
+using twoja_manufaktura.Models;
+
+namespace twoja_manufaktura.Infrastructure
+{
+    public class ProductDetailsDynamicNodeProvider : DynamicNodeProviderBase
+    {
+        private StoreContext db = new StoreContext(); 
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            var returnValue = new List<DynamicNode>();
+            foreach(Product p in db.Products)
+            {
+                DynamicNode node1 = new DynamicNode();
+                node1.Title = p.Name;
+                node1.Key = "Product_" + p.ProductId;
+                node1.ParentKey = "Genre_" + p.GenreId;
+                node1.RouteValues.Add("id", p.ProductId);
+                returnValue.Add(node1);
+
+
+            }
+            return returnValue;
+        }
+    }
+}
